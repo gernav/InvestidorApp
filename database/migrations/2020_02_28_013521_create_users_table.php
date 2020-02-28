456e46137c2c87d013532e44ba3b296701863c31
gernav <geraldonavarrete@gmail.com>
2020-02-28 00:39:24 -0300

    cria tabela User do banco de dados

---
+++ database/migrations/2020_02_28_013521_create_users_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Class CreateUsersTable.
+ */
+class CreateUsersTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table) {
+			$table->increments('id');
+			
+			//people data
+			$table->char('cpf', 11)->unique()->nullable();
+			$table->String('name', 50);
+			$table->char('phone', 11);
+			$table->date('birth')->nullable();
+			$table->char('gender', 1)->nullable();
+			$table->text('notes')->nullable();
+
+			//auth data
+			$table->String('email', 80)->unique();
+			$table->String('password', 254)->nullable();
+
+			//permissions
+			$table->String('status')->default('active');
+			$table->string('permission')->default('app.user');
+
+			$table->rememberToken();
+			$table->timestamps();
+			$table->softDeletes();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function(Blueprint $table){
+
+		});
+		
+		Schema::drop('users');
+	}
+}
